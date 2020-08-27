7618db2c996a370ea04613ad7fc05a02c9c921f1
fb <2442563148@qq.com>
2020-08-27 10:43:18 +0800

    token冲冲冲

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module go_server
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/garyburd/redigo v1.6.2 // indirect
+	github.com/gin-gonic/gin v1.6.3
+)
--- main.go
+++ main.go
@@ -2,38 +2,114 @@ package main
 
 import (
 	"fmt"
-
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
-
 	api "go_server/api"
+	"net/http"
+	"time"
 )
 
 var router *gin.Engine
 
 func init() {
 	router = gin.Default()
-	fmt.Println("初始化")
-
 }
 
 func main() {
-
 	// use注册中间件
 	router.Use(api.Middle)
+	router.Use(Cors())
 
-	router.GET("/ping", api.Ping)
-	router.GET("/welcome", api.Welcome)
-	router.GET("/form", api.Form)
-	router.POST("/upload", api.Upload)
-
-	router.GET("/Redirect1", api.Redirect1)
-	router.GET("/Redirect2", api.Redirect2)
-
-	// 托管静态目录
-	// router.StaticFS("/more_static", http.Dir("./"))
-	// 托管单个文件
-	// router.StaticFile("/logo.png", ".././express/dist/logo.png")
-
+	router.GET("/set", setting)
+	router.GET("/get", getting)
 	router.Run(":8080") // 监听并在 0.0.0.0:8080 上启动服务
+}
 
+//自定义一个字符串
+var jwtkey = []byte("www.topgoer.com")
+var str string
+
+type Claims struct {
+	UserId   uint
+	UserName string
+	jwt.StandardClaims
+}
+
+//颁发token
+func setting(ctx *gin.Context) {
+	expireTime := time.Now().Add(7 * 24 * time.Hour)
+	claims := &Claims{
+		UserId:   2,
+		UserName: "哇嗷",
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(), //过期时间
+			IssuedAt:  time.Now().Unix(),
+			Issuer:    "127.0.0.1",  // 签名颁发者
+			Subject:   "user token", //签名主题
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// fmt.Println(token)
+	tokenString, err := token.SignedString(jwtkey)
+	if err != nil {
+		fmt.Println(err)
+	}
+	str = tokenString
+	ctx.JSON(200, gin.H{"token": tokenString})
+}
+
+//解析token
+func getting(ctx *gin.Context) {
+
+	tokenString := ctx.Query("token")
+	fmt.Println(tokenString)
+	
+	// tokenString := ctx.GetHeader("Authorization")
+
+	if tokenString == "" {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足"})
+		ctx.Abort()
+		return
+	}
+
+	token, claims, err := ParseToken(tokenString)
+	if err != nil || !token.Valid {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足"})
+		ctx.Abort()
+		return
+	}
+	fmt.Println(claims.UserId)
+	ctx.JSON(200, gin.H{
+		"messeage": "成功",
+		"UserId":claims.UserId,
+		"UserName":claims.UserName,
+	})
+}
+
+func ParseToken(tokenString string) (*jwt.Token, *Claims, error) {
+	Claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, Claims, func(token *jwt.Token) (i interface{}, err error) {
+		return jwtkey, nil
+	})
+	return token, Claims, err
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		fmt.Println(method)
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
+		// 放行所有OPTIONS方法，因为有的模板是要请求两次的
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+
+		// 处理请求
+		c.Next()
+	}
 }
